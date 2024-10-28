afa204c7d245ecc821c61d60143b7f0ab920f36b
unknown <2btb16@IESRAMONLLULL.LOCAL>
2024-10-28 12:11:49 +0100

    fin de juego

--- cartas poo/juego.py
+++ cartas poo/juego.py
@@ -9,3 +9,26 @@ class Juego:
         manoJugador= Mano ()
         manoJugador.añadirCarta(self.baraja.sacarCarta())
         print(f"Tu mano es {manoJugador.cartas}")
+        print(f"Total: {manoJugador.valor}")
+
+        while manoJugador.valor < 21:
+            accion = input("¿Quieres PEDIR o PASAR?").lower()
+            if accion =="pedir":
+                manoJugador.añadirCarta(self.baraja.sacarCarta())
+                print(f"Tu mano es {manoJugador.cartas}")
+                print(f"Valor: {manoJugador.valor}")
+            else:
+                print(f"Tu puntuación es {manoJugador.valor}")
+                return
+        if manoJugador.valor == 21:
+            print("ENHORABUENA")
+        elif manoJugador.valor < 21:
+            print("No has llegado a 21")
+        else:
+            print("Te has pasado!")
+        
+
+if __name__ == "__main__":
+    print("JUEGO DE 21")
+    juego = Juego()
+    juego.jugar()
